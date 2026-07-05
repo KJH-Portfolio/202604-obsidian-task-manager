a6eb74dcc9ce9e37400274a8e8845b323490ceb7
KJH-15123 <kim50504376@gmail.com>
2026-07-05 19:00:41 +0900

    1

--- append_css.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const css = '\n\n/* 라이브 뷰 콜아웃 내부 체크박스 핀셋 보정 (테스트용) */\nbody .workspace-leaf-content .cm-line.cm-quote.HyperMD-task-line input[type="checkbox"] {\n    transform: translateY(10px) !important;\n}\n';
-fs.appendFileSync('styles.css', css, 'utf8');
-console.log('Appended test CSS');
--- fix_css_2.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let css = fs.readFileSync('styles.css', 'utf8');
-
-// Replace inset shadow with 1px border
-css = css.replace(/border: 1px solid transparent; box-shadow: inset 0 0 0 2px (rgba\([^)]+\));/g, 'border: 1px solid $1; box-shadow: none;');
-
-fs.writeFileSync('styles.css', css, 'utf8');
-console.log('CSS Border Fixed');
